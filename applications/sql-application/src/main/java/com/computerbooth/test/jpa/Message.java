493edf19e2a6724f1e0f445feb55d608a9f3e3ca
Endolf <endolf@computerbooth.com>
2017-05-12 18:30:07 +0100

    Fix unused imports

--- applications/sql-application/src/main/java/com/computerbooth/test/jpa/Message.java
+++ applications/sql-application/src/main/java/com/computerbooth/test/jpa/Message.java
@@ -2,8 +2,6 @@ package com.computerbooth.test.jpa;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.Instant;
