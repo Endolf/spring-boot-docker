70f692dada9dc8051564c8d2b0248b83cfc3318b
Endolf <endolf@computerbooth.com>
2017-05-17 21:55:27 +0100

    Add health URLs to prod

--- applications/front-end/src/environments/environment.prod.ts
+++ applications/front-end/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   name: "production",
-  messageServiceURLs: [{url: "mariadb/messages", source: "mariadb"},{url: "mongo/messages", source: "mongodb"}, {url:"cassandra/messages", source:"cassandra"}]
+  messageServiceURLs: [{url: "mariadb/messages", source: "mariadb", healthUrl: "mariadb/health"}
+                      ,{url: "mongo/messages", source: "mongodb", healthUrl: "mongo/health"}
+                      ,{url: "cassandra/messages", source:"cassandra", healthUrl: "cassandra/health"}]
 };
